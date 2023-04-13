97d13271e099a0ca3cfa90a053ba6fa97c48b69e
waweraty <35693847+waweraty@users.noreply.github.com>
2023-04-13 16:51:13 -0600

    Update SimpPred.py

--- pages/SimpPred.py
+++ pages/SimpPred.py
@@ -6,5 +6,12 @@ st.markdown("# Predictor Unitario")
 st.sidebar.header("Predictor Unitario")
 
 st.write(
-    """En esta aplicación se ingresa el título del programa para predecir su categoría"""
+    """En esta aplicación se ingresa el nombre del programa para predecir su categoría"""
 )
+
+text_input = st.text_input('Nombre del programa', placeholder="Favor de ingresar el nombre del programa")
+
+if text_input:
+        st.write("Nombre del programa: ", text_input)
+        st.write('Categoría predicha:')
+        st.write('Categoría real')
