0ea7ffff1f937670f01674eea42d6383739c4453
waweraty <35693847+waweraty@users.noreply.github.com>
2023-05-10 20:55:03 -0600

    Update Predictor Simple.py

--- pages/Predictor Simple.py
+++ pages/Predictor Simple.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import pickle
+
+@st.cache
+def load_model():
+        pkl = open("modelo_ifs.pkl", "rb")
+        model = pickle.load(pkl)
+
+        return model
+
+
+program_categorizer = load_model()
 
 st.set_page_config(page_title="Predictor Unitario", page_icon="1")
 
@@ -12,6 +23,8 @@ st.write(
 text_input = st.text_input('Nombre del programa', placeholder="Favor de ingresar el nombre del programa")
 
 if text_input:
+        pred=program_categorizer.categorize_program(text_input)
+
         st.write("Nombre del programa: ", text_input)
-        st.write('Categoría predicha:')
+        st.write('Categoría predicha: ', pred)
         st.write('Categoría real')
