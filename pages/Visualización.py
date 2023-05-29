a658e68e6ec623d74886b4d0565898eb52e53806
waweraty <35693847+waweraty@users.noreply.github.com>
2023-05-29 16:49:22 -0600

    Update Visualización.py

--- pages/Visualización.py
+++ pages/Visualización.py
@@ -94,6 +94,8 @@ def visKnear(data, texto, umap,cattype, fit, K=10):
     vectorizer = get_vect()
     u=umap.values
 
+    data
+
     textoarray = (vectorizer.transform(texto)).toarray()
     distarr=[braycurtis(r, textoarray[0]) for r in data.iloc[:,:-5].values]
     res = sorted(range(len(distarr)), key=lambda sub: distarr[sub])[:K]
