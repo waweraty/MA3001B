78b0cba78552d7041233a5dcfd41849ffee8b546
waweraty <35693847+waweraty@users.noreply.github.com>
2023-06-01 19:45:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,8 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.header('Equipo 1 🌼🌼🌼🌼')
-st.header("Predictor de Categoría de Programas Para Market Development Funds")
+st.header('🌼🌼 Equipo 1 🌼🌼')
+st.write('Ernesto Borbón, Gerardo Villegas, José de Jesús Gutiérrez, Luis Felipe Villaseñor')
+
+st.write("# Visualizador y Predictor de Categoría de Programas Para Market Development Funds")
 st.write('Favor de seleccionar la aplicación deseada en el menú de la izquierda')
